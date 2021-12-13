282f06416b14513bd6636993d35e7bab0c90dfe0
Romero Herreros, Alberto <alberto.romero@wolterskluwer.com>
2021-12-13 22:46:38 +0100

    Validaciones additionalData y tests unitarios

---
+++ Tests/a3innuva.TAA.Migration.SDK.Implementations.Tests/Validations/InputInvoiceAdditionalDataValidationTests.cs
@@ -0,0 +1,101 @@
+﻿namespace a3innuva.TAA.Migration.SDK.Implementations.Tests
+{
+    using a3innuva.TAA.Migration.SDK.Interfaces;
+    using FluentAssertions;
+    using System;
+    using System.Linq;
+    using Xunit;
+
+    [Trait("Unit test", "InputInvoiceAdditionalDataValidation")]
+    public class InputInvoiceAdditionalDataValidationTests
+    {
+        private InputInvoiceAdditionalDataValidation validation;
+
+        public InputInvoiceAdditionalDataValidationTests()
+        {
+            this.validation = new InputInvoiceAdditionalDataValidation();
+        }
+
+        ~InputInvoiceAdditionalDataValidationTests()
+        {
+            this.validation = null;
+        }
+
+        [Fact(DisplayName = "Validate succeed")]
+        public void Validate_succeed()
+        {
+            IInputInvoiceAdditionalData entity = this.CreateEntity();
+
+            var errors = this.validation.Validate(entity).ToList();
+
+            errors.Count.Should().Be(0);
+        }
+
+        [Fact(DisplayName = "Validate id failed")]
+        public void Validate_id_failed()
+        {
+            IInputInvoiceAdditionalData entity = this.CreateEntity();
+            entity.Id = Guid.Empty;
+
+            var errors = this.validation.Validate(entity);
+
+            errors.Should().Contain(x => !x.IsValid && x.Code == "Id");
+        }
+
+        [Fact(DisplayName = "Validate description length failed")]
+        public void Validate_description_length_failed()
+        {
+            IInputInvoiceAdditionalData entity = this.CreateEntity();
+            entity.Description = "1".PadLeft(501);
+
+            var errors = this.validation.Validate(entity);
+
+            errors.Should().Contain(x => !x.IsValid && x.Code == "El campo 'Descripción' tiene longitud incorrecta");
+        }
+
+        [Fact(DisplayName = "Validate DUA length failed")]
+        public void Validate_DUA_length_failed()
+        {
+            IInputInvoiceAdditionalData entity = this.CreateEntity();
+            entity.DuaDocumentId = "1".PadLeft(19);
+
+            var errors = this.validation.Validate(entity);
+
+            errors.Should().Contain(x => !x.IsValid && x.Code == "El campo 'Número de DUA' tiene longitud incorrecta");
+        }
+
+        [Fact(DisplayName = "Validate initial invoice number length failed")]
+        public void Validate_initial_invoice_number_length_failed()
+        {
+            IInputInvoiceAdditionalData entity = this.CreateEntity();
+            entity.InitialNumberOfDocument = "1".PadLeft(61);
+
+            var errors = this.validation.Validate(entity);
+
+            errors.Should().Contain(x => !x.IsValid && x.Code == "El campo 'Número de documento inicial' tiene longitud incorrecta");
+        }
+
+        [Fact(DisplayName = "Validate last invoice number length failed")]
+        public void Validate_last_invoice_number_length_failed()
+        {
+            IInputInvoiceAdditionalData entity = this.CreateEntity();
+            entity.LastNumberOfDocument = "1".PadLeft(61);
+
+            var errors = this.validation.Validate(entity);
+
+            errors.Should().Contain(x => !x.IsValid && x.Code == "El campo 'Número de documento final' tiene longitud incorrecta");
+        }
+
+        private IInputInvoiceAdditionalData CreateEntity()
+        {
+            return new InputInvoiceAdditionalData()
+            {
+                Id = Guid.NewGuid(),
+                Description = "desc",
+                DuaDocumentId = "Dua",
+                InitialNumberOfDocument = "1",
+                LastNumberOfDocument = "2"
+            };
+        }
+    }
+}
