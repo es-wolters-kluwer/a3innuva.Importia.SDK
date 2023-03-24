2815d7e8a00d6f05e16acfb9af8ffd40ec194af1
Henk Sandoval <henkalexander.sandoval@gmail.com>
2023-03-24 10:12:43 +0100

    refactor: Remove MigrationInfoValidation.cs class inline logic into MigrationInfoExtensions.cs

--- Importia.SDK/Implementations/a3innuva.Importia.SDK.Extensions/MigrationInfoExtensions.cs
+++ Importia.SDK/Implementations/a3innuva.Importia.SDK.Extensions/MigrationInfoExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace a3innuva.TAA.Migration.SDK.Extensions
 {
+	using System.Linq;
 	using System.Collections.Generic;
 	using a3innuva.TAA.Migration.SDK.Implementations;
 	using a3innuva.TAA.Migration.SDK.Interfaces;
@@ -8,9 +9,25 @@
 	{
 		public static IEnumerable<IValidationResult> GetValidations(this IMigrationInfo info)
 		{
-			var infoValidation = new MigrationInfoValidation(info);
-			return infoValidation.GetErrorValidations();
+			if (info.IsValid()) return Enumerable.Empty<IValidationResult>();
+
+			var listErrors = new List<IValidationResult>();
+			
+			if (!info.IsValidOrigin()) listErrors.Add(BuildValidationResult($"The {nameof(IMigrationInfo.Origin)} value is invalid"));
+			if (!info.IsValidType()) listErrors.Add(BuildValidationResult($"The {nameof(IMigrationInfo.Type)} value is invalid"));
+			if (!info.IsValidYear()) listErrors.Add(BuildValidationResult($"The {nameof(IMigrationInfo.Year)} value is invalid"));
+			if (!info.IsValidVatNumber()) listErrors.Add(BuildValidationResult($"The {nameof(IMigrationInfo.VatNumber)} value is invalid"));
+			if (!info.IsValidVersion()) listErrors.Add(BuildValidationResult($"The {nameof(IMigrationInfo.Version)} value is invalid"));
+
+			return listErrors;
 		}
+
+		private static ValidationResult BuildValidationResult(string error) =>
+			new ValidationResult()
+			{
+				Code = error,
+				Line = 0,
+				IsValid = false
+			};
 	}
 }
-
